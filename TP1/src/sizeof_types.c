bbe8820a284f1054699420d05c68620f148c0554
ouguemat <49242185+ouguemat@users.noreply.github.com>
2025-12-03 21:10:41 +0100

    Add sizeof_types.c to display data type sizes
    This program prints the sizes of various data types in C.

---
+++ TP1/src/sizeof_types.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    printf("Taille de signed char      : %zu octets\n", sizeof(signed char));
+    printf("Taille de unsigned char    : %zu octets\n", sizeof(unsigned char));
+    printf("Taille de char             : %zu octets\n", sizeof(char));
+
+    printf("Taille de signed short     : %zu octets\n", sizeof(signed short));
+    printf("Taille de unsigned short   : %zu octets\n", sizeof(unsigned short));
+
+    printf("Taille de signed int       : %zu octets\n", sizeof(signed int));
+    printf("Taille de unsigned int     : %zu octets\n", sizeof(unsigned int));
+
+    printf("Taille de signed long int  : %zu octets\n", sizeof(signed long int));
+    printf("Taille de unsigned long int: %zu octets\n", sizeof(unsigned long int));
+
+    printf("Taille de signed long long int  : %zu octets\n", sizeof(signed long long int));
+    printf("Taille de unsigned long long int: %zu octets\n", sizeof(unsigned long long int));
+
+    printf("Taille de float            : %zu octets\n", sizeof(float));
+    printf("Taille de double           : %zu octets\n", sizeof(double));
+    printf("Taille de long double      : %zu octets\n", sizeof(long double));
+
+    return 0;
+}
